6fd8f192720cdf7394a55c6abd7387c82e7e7a49
Renato Pavlic <renato.poslovni@gmail.com>
2020-06-19 20:15:03 +0200

    nav component

---
+++ frontend/src/components/navigationBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import style from "./index.css";
+
+function Navigacija() {
+  return (
+    <React.Fragment>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/" className="my-logo">Undabot</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto">
+            <Nav.Link href="/" className="my-link">Home</Nav.Link>
+            <Nav.Link href="/about" className="my-link">About Us</Nav.Link> 
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </React.Fragment>
+  )
+};
+
+export default Navigacija;
