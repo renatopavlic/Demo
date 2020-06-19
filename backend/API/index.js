60d32d9d485a4627f7b87509d8fe834add702941
Renato Pavlic <renato.poslovni@gmail.com>
2020-06-19 22:59:35 +0200

    almost finished

---
+++ backend/API/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res)=> {
+
+})
+app.post('/', (req, res) => {
+  let mailOk = false;
+  let messageOk = false;
+  if(req.body.email){
+    console.log('provjeri jel mail ok, npm email validate ili slicno trazi!');
+    mailOk = true; // ili false ako nije
+  }
+  if(req.body.message.length<30){
+    console.log("jel poruka dovoljno duga");
+    messageOk = true;
+  }
+  if(mailOk && messageOk){
+    res.status(200).json({message: "Your message has been sent!"})
+  } else {
+    res.status(422).json({
+      mailOk:mailOk,
+      messageOk: messageOk
+    })
+  }
+})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- frontend/src/components/inputForm.js
+++ frontend/src/components/inputForm.js
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
+import  { useHistory } from 'react-router-dom';
 
 import { Form, Button} from "react-bootstrap";
 
 function InputForm() {
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const history = useHistory()
+
+  const submit = e => {
+    setError(false);
+    e.preventDefault();
+    fetch(process.env.REACT_APP_ROOT+"/API/contact", {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        message:message
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.error){
+        setError(data.error.msg);
+      } else {
+        console.log("success");
+        if(data.success){
+          history.push('/')
+        } else {
+          console.log("no success? something is off, inspect!");
+          setError("unknown error, contact Renato")
+        }
+      }
+    })
+    .catch(err => {
+      console.error("Error:", err)
+    });
+  }
+
   return (
     <React.Fragment>
       <Form className="my-form">
         <Form.Group controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control type="email" placeholder="name@example.com" onChange={e=>setName(e.target.value)}/>
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlTextarea1">
           <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows="3" />
+          <Form.Control as="textarea" rows="3" onChange={e=>setName(e.target.value)} />
         </Form.Group>
         <Button type="submit" variant="primary">Submit</Button>
       </Form>
